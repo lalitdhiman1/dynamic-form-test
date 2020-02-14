06c83bd06eee7a0051cff17e582d6b2e53f55672
Lalit Dhiman <lalitdhiman1@gmail.com>
2020-02-14 22:09:24 +0530

    upload

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/dynamicForm')); 
+app.use(express.static(__dirname + '/dist/dynamic-form-test')); 
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/dynamicForm/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/dynamic-form-test/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
